3e038c6bf7359a2595f2b0cd2260a9759a751e8e
Raul Barrios Siles <raulbarriossiles@gmail.com>
2020-02-26 18:23:28 +0100

    fix(): you could listen your own audio

--- P2P-example/client/src/view/webRTC.view.js
+++ P2P-example/client/src/view/webRTC.view.js
@@ -10,6 +10,7 @@ class webRTCView {
     };
 
     this.localStream = null;
+    this.localStreamEmited = null;
     this.optionLocalStream = {
       audio: true,
       video: true
@@ -33,11 +34,14 @@ class webRTCView {
   bindAddTrackToWebRTC(handler) {
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: true })
-      .then(localStream => {
-        this.localStream = localStream;
-        this.GUI.localVideo.srcObject = this.localStream;
-        handler(this.localStream);
+      .then(localStreamEmited => {
+        this.localStreamEmited = localStreamEmited;
+        handler(this.localStreamEmited);
       });
+    navigator.mediaDevices.getUserMedia({ video: true }).then(localStream => {
+      this.localStream = localStream;
+      this.GUI.localVideo.srcObject = this.localStream;
+    });
   }
 
   bindStartCall(handler) {
@@ -83,11 +87,12 @@ class webRTCView {
     this._toogleOptionCall('video');
     this.GUI.buttonVideo.classList.toggle('on');
     this.localStream.getVideoTracks()[0].enabled = this.optionLocalStream.video;
+    this.localStreamEmited.getVideoTracks()[0].enabled = this.optionLocalStream.video;
   };
 
   clickAudioButton = () => {
     this._toogleOptionCall('audio');
     this.GUI.buttonAudio.classList.toggle('on');
-    this.localStream.getAudioTracks()[0].enabled = this.optionLocalStream.audio;
+    this.localStreamEmited.getAudioTracks()[0].enabled = this.optionLocalStream.audio;
   };
 }
